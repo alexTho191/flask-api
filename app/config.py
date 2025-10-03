0dc9708f6ecb80e94c0df5e30c1e7899201dc000
alexTho191 <alxndr.11deas@gmail.com>
2025-10-02 19:40:35 -0500

    configdb

--- app/config.py
+++ app/config.py
@@ -1,6 +1,4 @@
-import os
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'postgresql://alexcaviedes:fCQBV5KgVaZwaO4jk0D5CTFnjWHESNB4@dpg-d3ffup6mcj7s738tsf9g-a.oregon-postgres.render.com/restaurantdb_dpea'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
