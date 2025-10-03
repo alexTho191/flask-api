158422abe8d7849f8c83f3867245f9e1794bac32
alexTho191 <alxndr.11deas@gmail.com>
2025-10-02 19:52:49 -0500

    config completa

--- app/config.py
+++ app/config.py
@@ -1,4 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # carga variables del .env
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://alexcaviedes:fCQBV5KgVaZwaO4jk0D5CTFnjWHESNB4@dpg-d3ffup6mcj7s738tsf9g-a.oregon-postgres.render.com/restaurantdb_dpea'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///local.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
